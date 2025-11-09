b128461cd0a6f05b08f15c606c52fc3d4e434539
janlevea <60253942+janlevea@users.noreply.github.com>
2025-11-09 17:23:43 +0200

    Exercise 09 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (
+        num1 < 0 || num2 < 0 ||
+        !Number.isInteger(num1) || !Number.isInteger(num2)
+    ) {
+        return "ERROR";
+    }
+    if (!(num1 <= num2)) { 
+        // if second number is bigger than the first one
+        // reverse them
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
+    for (num1; num1 <= num2; num1++) {
+        sum += num1;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
