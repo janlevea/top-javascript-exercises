0dd19d6f74e23e6f7200863b8dedadb0a2b8fd00
janlevea <60253942+janlevea@users.noreply.github.com>
2025-11-09 17:43:36 +0200

    Exercise 10 done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // return true for a leap year
+    // years divisible by 4 = leap years
+    // however, years divisible by 100 != leap years
+    // unless they are divisible by 400
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year) 
+        {
+            if (year % 400 === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+    // return false for non leap year
 
 };
 
