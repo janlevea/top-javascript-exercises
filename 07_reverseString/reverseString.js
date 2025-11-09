abb3bbdca9c0cbdb2019bf843d58b1e93cf27b19
janlevea <60253942+janlevea@users.noreply.github.com>
2025-11-09 16:55:50 +0200

    Exercises 06-08 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    strLen = string.length;
+    reversedStr = "";
+    for (let i = strLen - 1; i >= 0; i--) {
+        reversedStr += string[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach((arg) => {
+        let ixToRemove = array.indexOf(arg);
+        while (ixToRemove !== -1) {
+            array.splice(ixToRemove, 1);
+            ixToRemove = array.indexOf(arg);
+        }
+    })
+    return array;
 };
 
 // Do not edit below this line
