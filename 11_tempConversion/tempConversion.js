88be5a95a9b7222a46f17344bde9a0b6ffc13e9c
janlevea <60253942+janlevea@users.noreply.github.com>
2025-11-09 18:00:05 +0200

    Exercise 11 and top/lessons/foundations-loops-and-arrays done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // From fahrenheit to celsius
+  let celsius = (fahrenheit - 32) * 5/9;
+  // Round to 1 decimal point
+  // toFixed returns a string + before variable name converts it into number
+  celsius = +celsius.toFixed(1);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // From celsius to fahrenheit
+  let fahrenheit = (celsius * 9/5 + 32);
+  // Round to 1 decimal point
+  fahrenheit = +fahrenheit.toFixed(1);
+  return fahrenheit;
 };
 
 // Do not edit below this line
