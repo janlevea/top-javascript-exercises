c52d322028467ad6081e605c161486f45b669dac
janlevea <60253942+janlevea@users.noreply.github.com>
2026-01-23 09:49:10 +0200

    Exercises 12 and 13 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNums) {
+	return arrayOfNums.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNums) {
+  return arrayOfNums.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove punctuation
+    str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    // Remove spaces
+    str = str.replaceAll(" ", "");
+    // Ignore casing
+    str = str.toLowerCase();
+    // Get length
+    lengthOfStr = str.length;
 
+    // Create reversed string
+    let reversedString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    // Return true if palindrome
+    return reversedString === str;
 };
 
 // Do not edit below this line
