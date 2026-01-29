78edf644375d6f35a08483885e26bfa627e0bb73
janlevea <60253942+janlevea@users.noreply.github.com>
2026-01-29 17:28:18 +0200

    Exercises 14-16 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    // Convert a number given as string to a number
+    number = parseInt(number);
 
+    // Variables for calculating
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+
+    // Don't accept negatives
+    if (number < 0) return "OOPS";
+    else if (number === 0) return 0;
+    else if (number === 1 || number === 2) return 1;
+    else {
+        for (let i = 2; i <= number; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,38 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
+let ageOfOldestPerson = 0;
+let oldestPerson;
 
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let ageOfOldestPerson = 0;
+    let oldestPerson;
+
+    for (let i = 0; i < people.length; i++) {
+        if (ageOfOldestPerson === 0) {
+            if (people[i].yearOfDeath === undefined) {
+                ageOfOldestPerson = currentYear - people[i].yearOfBirth;
+                oldestPerson = people[i];
+            }
+            else {
+                ageOfOldestPerson = people[i].yearOfDeath - people[i].yearOfBirth;
+                oldestPerson = people[i];
+            }
+        }
+        else {
+            let age = 0;
+            if (people[i].yearOfDeath === undefined) {
+                age = currentYear - people[i].yearOfBirth;
+            }
+            else {
+                age = people[i].yearOfDeath - people[i].yearOfBirth;
+            }
+            if (age > ageOfOldestPerson) {
+                ageOfOldestPerson = age;
+                oldestPerson = people[i];
+            }
+        }
+    }
+    return oldestPerson; 
 };
 
 // Do not edit below this line
